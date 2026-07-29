9084058a351f96de69b284c33777db6e713f9890
Ajay Sharma <sharmaa8203@gmail.com>
2026-07-29 17:41:53 +0530

    update loading

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ connectdatabse()
     const server = http.createServer(app);
     const io = new Server(server, {
       cors: {
-        // origin: "https://frontend-tau-ivory-25.vercel.app",
-        origin: "http://localhost:5173",
+        origin: "https://frontend-tau-ivory-25.vercel.app",
+        // origin: "http://localhost:5173",
         credentials: true,
       },
     });
