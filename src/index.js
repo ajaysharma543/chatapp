f7f9fe71ff3d4cc406ddf2193834368c71509d9f
Ajay Sharma <sharmaa8203@gmail.com>
2026-08-21 17:55:54 +0530

    added

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ connectdatabse()
     const server = http.createServer(app);
     const io = new Server(server, {
       cors: {
-        origin: "https://frontend-tau-ivory-25.vercel.app",
+        origin: "https://frontend-tau-puce-23.vercel.app",
         // origin: "http://localhost:5173",
         credentials: true,
       },
